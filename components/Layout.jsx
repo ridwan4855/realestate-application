70cd82b348306ff5eb5a1e349a2859937ea9b280
Ridwan Maulana Fatah <maulanar2521@gmail.com>
2022-06-02 10:18:03 +0700

    Adding Footer to the page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 const Layout = ({ children }) => (
   <>
     <Head>
@@ -12,7 +13,9 @@ const Layout = ({ children }) => (
         <Navbar />
       </header>
       <main>{children}</main>
-      <footer>Footer</footer>
+      <footer>
+        <Footer />
+      </footer>
     </Box>
   </>
 );
